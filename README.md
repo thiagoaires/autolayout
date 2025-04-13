f013f5d86e300c91ad5e867976e1458fd1cdbfa8
Thiago Aires da Silva <makarrao@gmail.com>
2025-04-12 22:51:33 -0300

    Inicio do projeto

---
+++ README.md
@@ -0,0 +1,1 @@
+# IOS - Auto Layout
---
+++ autolayout/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  autolayout
+//
+//  Created by Thiago Aires da Silva on 12/04/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
